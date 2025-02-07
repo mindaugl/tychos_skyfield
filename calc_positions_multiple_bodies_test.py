0c046d8a5d216cd532d8a44251de31b6588b3b73
mindaugl <mindelek@gmail.com>
2025-02-07 10:50:51 +0700

    Add move_speed_basic() to properly rotate earth position initially. Add speed internal parameter. Speed tested with tyhosium.

--- calc_positions_multiple_bodies_test.py
+++ calc_positions_multiple_bodies_test.py
@@ -1,5 +1,6 @@
 """
 Test script showing how to set up multiple object(planet) system.
+Planet speed is added.
 The test agrees with the Tychosium - both planet locations and quaternions were tested.
 """
 from scipy.spatial.transform import Rotation as R
@@ -33,6 +34,7 @@ class PlanetObj:
         rotation
         location
         center
+        speed
         radius_vec
         children
 
@@ -40,14 +42,16 @@ class PlanetObj:
     -------
         __init__
         move_planet
+        move_planet_basic
         add_child
 
     Notes
     -----
     move_planet() method needs to be called for parent first and only then for child.
+    speed = 1/period(years) * 2pi, represents rotation radians per year
     """
-    def __init__(self, orbit_radius=100.0, orbit_center = OrbitCenter(),
-                 orbit_tilt = OrbitTilt(), start_pos=20.0):
+    def __init__(self, orbit_radius = 100.0, orbit_center = OrbitCenter(),
+                 orbit_tilt = OrbitTilt(), start_pos = 20.0, speed = 0.0):
 
         self.start_pos = start_pos
         self.rotation = (R.from_euler('x', orbit_tilt.x, degrees = True) *
@@ -56,24 +60,36 @@ class PlanetObj:
         self.center = (np.array([orbit_center.x, orbit_center.y, orbit_center.z]).
                        astype(np.float64))
         self.radius_vec = np.array([orbit_radius, 0.0, 0.0])
+        self.speed = speed / (2 * np.pi)
         self.children = []
 
-    def move_planet(self, speed, pos):
+    def move_planet(self, pos):
         """
-        Moves planet by specified pos and speed
-        :param speed: float
-            Moving speed multiplier.
+        Moves planet by specified pos and speed.
+        NOTE: only can use function once, as everytime it modifies values
         :param pos: float
             Position in degrees to rotate around y-axis
         :return:
         """
-        self.rotation = self.rotation * R.from_euler('y', speed * pos - self.start_pos,
+        self.rotation = self.rotation * R.from_euler('y', self.speed * pos - self.start_pos,
                                                      degrees = True)
         radius_rotated = self.rotation.apply(self.radius_vec)
         self.location = self.center + radius_rotated
         for child in self.children:
             child.rotation = self.rotation * child.rotation
             child.center = self.center + self.rotation.apply(self.radius_vec + child.center)
+
+    def move_planet_basic(self, pos):
+        """
+        Moves planet by specified pos, assuming speed = 0 and start_pos = 0.
+        Can call this function multiple times - it does not modify children
+        :param pos: float
+            Position in degrees to rotate around y-axis
+        :return:
+        """
+        self.rotation = self.rotation * R.from_euler('y', pos, degrees = True)
+        radius_rotated = self.rotation.apply(self.radius_vec)
+        self.location = self.center + radius_rotated
 
     def add_child(self, child_obj):
         """
@@ -89,16 +105,16 @@ class PlanetObj:
 # both quaternions and positions agree with tychosium
 
 sun_temp_def = PlanetObj(35, OrbitCenter(10, 20, -10),
-                    OrbitTilt(30, 60), 180)
+                    OrbitTilt(30, 60), 180, 0)
 sun_temp = PlanetObj(100, OrbitCenter(25, 35, -10),
-                OrbitTilt(15, 30), 15)
+                OrbitTilt(15, 30), 15, 0)
 sun_temp_def.add_child(sun_temp)
-sun_temp_def.move_planet(1, 0)
+sun_temp_def.move_planet(0)
 print("sun_temp before move:")
 print("location:", sun_temp.location)
 print("rotation vector:", sun_temp.rotation.as_rotvec())
 print("quaternion:", sun_temp.rotation.as_quat())
-sun_temp.move_planet(1, 0)
+sun_temp.move_planet(0)
 
 print("sun_temp_def:")
 print("location:", sun_temp_def.location)
@@ -114,24 +130,25 @@ print("quaternion:", sun_temp.rotation.as_quat())
 # both quaternions and positions agree with tychosium
 
 earth = PlanetObj( 37.8453, OrbitCenter(0, 0, 0),
-                      OrbitTilt(0, 0), 0)
+                      OrbitTilt(0, 0), 0, -0.0002479160869310127)
 mer_def_a = PlanetObj(100, OrbitCenter(-6.9, -3.2, 0),
-                      OrbitTilt(0, 0), 0)
+                      OrbitTilt(0, 0), 0, 2 * np.pi)
 mer_def_b = PlanetObj(0, OrbitCenter(0, 0, 0),
-                      OrbitTilt(-1.3, 0.5), 33)
+                      OrbitTilt(-1.3, 0.5), 33, -2 * np.pi)
 mer = PlanetObj(38.710225, OrbitCenter(0.6, 3, -0.1),
-                OrbitTilt(3, 0.5), -180.8)
+                OrbitTilt(3, 0.5), -180.8, 26.08763045)
 
 earth.add_child(mer_def_a)
 mer_def_a.add_child(mer_def_b)
 mer_def_b.add_child(mer)
 
-position = 0 # the new position in degrees
+position = 0.25 * 360 # the new position in degrees. For speed = 2pi, position repeats at pos = 360
 
-earth.move_planet(1, 90 + position)
-mer_def_a.move_planet(1, position)
-mer_def_b.move_planet(1, position)
-mer.move_planet(1, position)
+earth.move_planet_basic(90)
+earth.move_planet(position)
+mer_def_a.move_planet(position)
+mer_def_b.move_planet(position)
+mer.move_planet(position)
 print("")
 
 print("earth:")
@@ -139,17 +156,17 @@ print("location:", earth.location)
 print("rotation vector:", earth.rotation.as_rotvec())
 print("quaternion:", earth.rotation.as_quat())
 
-print("mer_def_a:")
+print("mercury_def_a:")
 print("location:", mer_def_a.location)
 print("rotation vector:", mer_def_a.rotation.as_rotvec())
 print("quaternion:", mer_def_a.rotation.as_quat())
 
-print("mer_def_b:")
+print("mercury_def_b:")
 print("location:", mer_def_b.location)
 print("rotation vector:", mer_def_b.rotation.as_rotvec())
 print("quaternion:", mer_def_b.rotation.as_quat())
 
-print("mer:")
+print("mercury:")
 print("location:", mer.location)
 print("rotation vector:", mer.rotation.as_rotvec())
 print("quaternion:", mer.rotation.as_quat())
